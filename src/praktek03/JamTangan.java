d932a4388c6dc23d4bf73ef33e99f396d4f5565d
Galuh Farial Ramadhana <galuhramadhana15@gmail.com>
2018-04-05 16:38:07 +0700

    buat method cetak info()

--- src/praktek03/JamTangan.java
+++ src/praktek03/JamTangan.java
@@ -1,5 +1,17 @@
 package praktek03;
 
 public class JamTangan {
+    String merk;
+    String ukuran;
+    String tahun;
+    String bahan;
+    String bentuk;
+    double harga;
     
+    
+    void cetakInfo(){
+        System.out.println("Merk /t:"+merk+"/n"+"Ukuran /t:"+ukuran+"/n"+
+                "Tahun /t:"+tahun+"/n"+"Bahan /t:"+bahan+"/n"+"Bentuk /t:"+bentuk+"/n"+
+                "Bahan /t:"+bahan);
+    }
 }
